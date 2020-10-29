fa2a6c2aff6f57e8031eb5dda9bd2fb32a33d2aa
Derek Baker <derek.baker89@gmail.com>
2020-10-29 11:44:56 +0000

    created html skeleton

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import { ReactComponent as Logo } from './Assets/hackney-logo.svg';
 function App() {
   return (
     <div>
-      <Logo />
+      <header>
+        <Logo />
+      </header>
+
+      <section>
+        <p>Are you a</p>
+        <button>Visitor</button>
+        <button>Employee</button>
+      </section>
+
+      <footer>
+        <p>All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence v3.0</a>, except where otherwise stated</p>
+      </footer>
+      
     </div>
   );
 }
