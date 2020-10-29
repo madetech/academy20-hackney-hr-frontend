0072985bb4482dd7a950fbb027f34cbc984d931a
Derek Baker <derek.baker89@gmail.com>
2020-10-29 16:36:43 +0000

    added form to visitor page

--- src/Pages/Visitor.js
+++ src/Pages/Visitor.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import "./Visitor.css"
 
 export default function VisitorPage() {
     return(
         <div>
-            <h1>This is the visitor page</h1>
+            <section>
+                <div className="welcome-text">
+                    <h1>Visitor Registration</h1>
+                    <p>Please enter your details and an employee will be with you shortly.</p>
+                </div>
+                <form>
+                    <label for="fname">First name:</label><br/>
+                    <input type="text" id="fname" name="fname"/><br/>
+                    <label for="lname">Last name:</label><br/>
+                    <input type="text" id="lname" name="lname"/><br/>
+                    <label for="ename">Employee name:</label><br/>
+                    <input type="text" id="ename" name="ename"/><br/>
+                    <label for="cnumber">Contact number:</label><br/>
+                    <input type="text" id="cnumber" name="cnumber"/><br/>
+                    <label for="time">Time of arrival:</label><br/>
+                    <input type="text" id="time" name="time"/><br/><br/>
+                    <input type="submit" value="Submit"></input>
+                </form>
+            </section>
+            
         </div>
     )
 }
