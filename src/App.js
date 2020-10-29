dc8fd54eeede9901196e6509f699a1748d902e12
bella-cockrell <arabella.cockrell@gmail.com>
2020-10-29 15:11:24 +0000

    Adding comment to componetize button later

--- src/App.js
+++ src/App.js
@@ -15,11 +15,14 @@ function App() {
         </header>
 
         <section>
+          <div className="welcome-text">
           <h1>Welcome to the Hackney Council HR System.</h1>
-          <p>If you are a visitor with a pre-booked appointment, please register your visit by clicking the button below.</p>
+          <p>If you are a visitor with a pre-booked appointment, please register your visit by clicking the button below. Once you have registered your visit, the person who you booked a visit with will soon come and collect you.</p>
           <p>If you are an employee of Hackney Council, please login using the button below.</p>
+          </div>
           
           <div className="button-container"> 
+          {/*turn these buttons into components for reuseability*/}
             <button>
               Register your visit
               <svg width="17.5" height="19" viewBox="0 0 33 40">
