b9d0729d57393fbdbbbb7d71dc3825de48158f85
bella-cockrell <arabella.cockrell@gmail.com>
2020-10-29 10:52:14 +0000

    Adding svg logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import logo from "./assets/logo.svg"
+import { ReactComponent as Logo } from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo}/>
+    <div>
+      <Logo />
     </div>
   );
 }
