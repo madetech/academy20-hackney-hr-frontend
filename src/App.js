b98d0e500ea39de8f62151a58a7e15c0d7940dc5
Derek Baker <derek.baker89@gmail.com>
2020-10-29 12:12:36 +0000

    added footer and header styling

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import { ReactComponent as Logo } from './Assets/hackney-logo.svg';
 function App() {
   return (
     <div>
-      <header>
-        <Logo />
-      </header>
+      
 
-      <section>
-        <p>Are you a</p>
-        <button>Visitor</button>
-        <button>Employee</button>
-      </section>
+      <body>
+        <header>
+          <Logo />
+        </header>
+        <section>
+          <p>Are you a</p>
+          <button>Visitor</button>
+          <button>Employee</button>
+        </section>
+        <footer>
+          <p>All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence v3.0</a>, except where otherwise stated</p>
+        </footer>
+      </body>
 
-      <footer>
-        <p>All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence v3.0</a>, except where otherwise stated</p>
-      </footer>
+      
+
+      
       
     </div>
   );
