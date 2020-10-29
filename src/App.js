39aecb72f1ee3d3c7d928a533133949b8c4c3478
bella-cockrell <arabella.cockrell@gmail.com>
2020-10-29 15:41:06 +0000

    Adding route for Visitor page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { ReactComponent as Logo } from "./Assets/hackney-logo.svg";
 
+import VisitorPage from "./Pages/Visitor";
+
 function App() {
   return (
     <Router>
@@ -16,7 +18,9 @@ function App() {
             <div className="header-lines"></div>
           </header>
 
-          <Switch>
+            <Switch>
+              <Route exact path="/visitor" component={VisitorPage}/>
+            
             <section>
               <div className="welcome-text">
               <h1>Welcome to the Hackney Council HR System.</h1>
@@ -26,12 +30,15 @@ function App() {
               
               <div className="button-container"> 
               {/*turn these buttons into components for reuseability*/}
+              <Link to="/visitor">
                 <button>
                   Register your visit
                   <svg width="17.5" height="19" viewBox="0 0 33 40">
                     <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z"/>
                   </svg>
                 </button>
+              </Link>
+                
                 <button>
                   Employee login
                   <svg width="17.5" height="19" viewBox="0 0 33 40">
@@ -40,10 +47,7 @@ function App() {
                 </button>
               </div>
             </section>
-
-          </Switch>
-
-          
+            </Switch>
 
           <footer>
             <p>All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence v3.0</a>, except where otherwise stated</p>
