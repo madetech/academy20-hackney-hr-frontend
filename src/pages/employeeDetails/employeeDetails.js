a4797ce4160ba30eede64ce531dd61faeabb122e
bella-cockrell <arabella.cockrell@gmail.com>
2020-11-02 13:51:07 +0000

    Adding extra details:

--- src/pages/employeeDetails/employeeDetails.js
+++ src/pages/employeeDetails/employeeDetails.js
@@ -9,8 +9,13 @@ export default function EmployeeDetails() {
                 <div className="welcome-text">
                     <h1>Employee Details</h1>
                     <p>Name: {users[0].first_name} {users[0].last_name}</p>  
-                    <p>Job title: {users[0]["Job Title"]}</p>  
-
+                    <p>Job title: {users[0]["Job Title"]}</p>
+                    <p>Email: {users[0]["contact email"]}</p> 
+                    <p>Salary: {users[0].Salary}</p> 
+                    <p>Office: {users[0]["Office Location"]}</p>
+                    <p>Contact information: <br/>{users[0]["Street Number"]}
+                        <br/>{users[0]["Street Name"]}
+                        <br/>{users[0]["Post Code"]}</p>
                     {console.log(users[0])}
                     {/*we will need to add id as another param in employee*/}
                     {/* {users.map(employee => (
