22ed91bb150187833a39ba8b499ba4a60154ff9a
bella-cockrell <arabella.cockrell@gmail.com>
2020-11-02 13:41:25 +0000

    Adding job title test, passes

--- src/pages/employeeDetails/__test__/employeeDetails.test.js
+++ src/pages/employeeDetails/__test__/employeeDetails.test.js
@@ -6,9 +6,14 @@ describe('Employee Details Page', () => {
     test("renders without crashing", () => {
         render(<EmployeeDetails />);
     })
-    test('renders the first name of the first employee', () => {
+    test('renders the first name of the employee', () => {
         render(<EmployeeDetails />);
-        const linkElement = screen.getByText(/Lenette Enrique/i);
+        const linkElement = screen.getByText(/Lenette/i);
+        expect(linkElement).toBeInTheDocument();
+      });
+      test('renders the job title of the first employee', () => {
+        render(<EmployeeDetails />);
+        const linkElement = screen.getByText(/Assistant Manager/i);
         expect(linkElement).toBeInTheDocument();
       });
 })
--- src/pages/employeeDetails/employeeDetails.js
+++ src/pages/employeeDetails/employeeDetails.js
@@ -8,11 +8,14 @@ export default function EmployeeDetails() {
         <div>
                 <div className="welcome-text">
                     <h1>Employee Details</h1>
+                    <p>Name: {users[0].first_name} {users[0].last_name}</p>  
+                    <p>Job title: {users[0]["Job Title"]}</p>  
+
+                    {console.log(users[0])}
                     {/*we will need to add id as another param in employee*/}
-                    {users.map(employee => (
+                    {/* {users.map(employee => (
                         <p key={employee}>{employee.first_name}</p>
-                    ))}
-                    <p>Lenette Enrique</p> 
+                    ))} */}
                 </div>
         </div>
     )
