3f26a7bfa0220c8189d2b703b73b1a91e0d29e18
bella-cockrell <arabella.cockrell@gmail.com>
2020-11-02 12:29:51 +0000

    first names rendering on employee details page

--- src/pages/employeeDetails/employeeDetails.js
+++ src/pages/employeeDetails/employeeDetails.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import "./employeeDetails.css"
+import "./employeeDetails.css";
+import getUsers from "../../getUsers";
 
 export default function EmployeeDetails() {
     return(
         <div>
                 <div className="welcome-text">
+                    {console.log(getUsers)}
                     <h1>Employee Details</h1>
-                    <p>This is who you are.</p>
+                    {getUsers().map(employee => (
+                        <p>{employee.first_name}</p>
+                    ))}
                 </div>
         </div>
     )
