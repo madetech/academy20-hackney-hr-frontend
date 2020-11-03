bde6c5ef69cbd4d28ff3d771c03d0de0adff9f50
Derek Baker <derek.baker89@gmail.com>
2020-11-03 11:59:32 +0000

    add loginname test

---
+++ src/components/loginName/__test__/loginName.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import loginName from "../loginName";
+
+let container = null;
+beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null
+})
+
+
