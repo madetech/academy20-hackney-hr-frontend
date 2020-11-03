9575491e57867b445602053e36552ad020cc44fb
bella-cockrell <arabella.cockrell@gmail.com>
2020-11-03 15:36:13 +0000

    Adding test

--- src/components/headerBanner/__test__/headerBanner.test.js
+++ src/components/headerBanner/__test__/headerBanner.test.js
@@ -1,5 +1,18 @@
-// import React from 'react';
-// import { render, unmountComponentAtNode } from "react-dom";
-// import { screen } from '@testing-library/react';
-// import { act } from "react-dom/test-utils";
-// import headerBanner from "../headerBanner";
+import React from 'react';
+import { render, unmountComponentAtNode } from "react-dom";
+import { screen } from '@testing-library/react';
+import { act } from "react-dom/test-utils";
+import headerBanner from "../headerBanner";
+
+let container = null;
+beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null
+});
+
