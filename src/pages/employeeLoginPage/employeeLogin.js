634c3bb2e39d20d62c8314b0f4091e46884e266d
bella-cockrell <arabella.cockrell@gmail.com>
2020-11-04 20:51:03 +0000

    adding code comments and removing snarky comment for user testing tomorrow

--- src/pages/employeeLoginPage/employeeLogin.js
+++ src/pages/employeeLoginPage/employeeLogin.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from "react";
 import "./employeeLogin.css"
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import EmployeeDetails from "../employeeDetailsPage/employeeDetails";
+import Login from "../../components/login/login"
 import SubmitButton from '../../components/submitButton/submitButton';
 
-export default function EmployeeLogin() {
+export default function EmployeeLoginPage() {
+    const [isLoggedIn, setIsLoggedIn ] = useState(false);
+    const [id, setId ] = useState("");
+
+    // useEffect(() => {
+    //     console.log(isLoggedIn);
+    // }, [setIsLoggedIn])
+
     return(
         <div>
             <Router>
                 <Switch>
-                        <Route exact path="/details" component={EmployeeDetails}/>
+                        {/* <Route 
+                            exact route="/login"
+                            render={props => (isLoggedIn ?
+                                <Redirect to="/details"/> :
+                                <Login {...props} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} setId={setId} />
+                            )}
+                        /> */}
+                        {/* <Route exact route="/details" component={EmployeeDetails}/> */}
                         <section>
                             <div className="welcome-text">
                                 <h1>Employee Login</h1>
-                                <p>Please enter your credentials. If you have forgotten your password, too bad.</p>
+                                <p>Please enter your credentials.</p>
                             </div>
                             <form data-testid="visitor-form">
                                 <label htmlFor="email">Email:</label><br/>
