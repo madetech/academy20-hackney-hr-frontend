87845f8f933ffbc8ffc63b05f5d1d985d618b06c
bella-cockrell <arabella.cockrell@gmail.com>
2020-11-09 16:50:34 +0000

    Adding form submit pop up

--- src/components/visitorForm/visitorForm.js
+++ src/components/visitorForm/visitorForm.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState} from 'react';
 import SubmitButton from '../submitButton/submitButton';
 
 export default function VisitorForm() {
+    const [submit, setSubmit] = useState("")
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setSubmit("Form submitted!")
+    }
     return (
         <div>
-            <form data-testid="visitor-form">
+            <form onSubmit={handleSubmit} data-testid="visitor-form">
                 <label htmlFor="fname">Your first name:</label><br/>
                 <input type="text" id="fname" name="fname"/><br/>
                 <label htmlFor="lname">Your last name:</label><br/>
@@ -15,7 +21,7 @@ export default function VisitorForm() {
                 <input type="text" id="ename" name="ename"/><br/>
                 <label htmlFor="time">Time of arrival (24hr):</label><br/>
                 <input className="input-time" type="time" id="time" name="time"/><br/><br/>
-                <SubmitButton/>
+                <SubmitButton/><div>{submit}</div>
             </form>
     </div>
     )
