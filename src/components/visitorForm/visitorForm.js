5923183ffc4eef0960155f0d1ae7f8073f18f892
Ben <ben.dalton@madetech.com>
2020-11-09 16:33:39 +0000

    adjusted time input field for mobile responsiveness

--- src/components/visitorForm/visitorForm.js
+++ src/components/visitorForm/visitorForm.js
@@ -5,16 +5,16 @@ export default function VisitorForm() {
     return (
         <div>
             <form data-testid="visitor-form">
-                <label htmlFor="fname">First name:</label><br/>
+                <label htmlFor="fname">Your first name:</label><br/>
                 <input type="text" id="fname" name="fname"/><br/>
-                <label htmlFor="lname">Last name:</label><br/>
+                <label htmlFor="lname">Your last name:</label><br/>
                 <input type="text" id="lname" name="lname"/><br/>
-                <label htmlFor="ename">Employee name:</label><br/>
-                <input type="text" id="ename" name="ename"/><br/>
-                <label htmlFor="cnumber">Contact number:</label><br/>
+                <label htmlFor="cnumber">Your contact number:</label><br/>
                 <input type="text" id="cnumber" name="cnumber"/><br/>
-                <label htmlFor="time">Time of arrival:</label><br/>
-                <input type="text" id="time" name="time"/><br/><br/>
+                <label htmlFor="ename">The employee you are<br/>visiting:</label><br/>
+                <input type="text" id="ename" name="ename"/><br/>
+                <label htmlFor="time">Time of arrival (24hr):</label><br/>
+                <input className="input-time" type="time" id="time" name="time"/><br/><br/>
                 <SubmitButton/>
             </form>
     </div>
