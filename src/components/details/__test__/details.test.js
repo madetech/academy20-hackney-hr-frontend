e3e225150620094c63aa30574f920e1d2e007e0a
bella-cockrell <arabella.cockrell@gmail.com>
2020-11-13 10:49:17 +0000

    Adding start of API test

--- src/components/details/__test__/details.test.js
+++ src/components/details/__test__/details.test.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+
 import Details from "../details";
+
+const server = setupServer(
+    rest.get('/details'), (req, res, ctx) => {
+        return res(ctx.json( { first_name: "Maribeth" }))
+    }
+)
 
 let container = null;
 beforeEach(() => {
