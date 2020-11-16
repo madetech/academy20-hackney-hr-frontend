e2a30d0ba8ff609e80de80c5410e458afc9ef7b8
Ben <ben.dalton@madetech.com>
2020-11-16 11:33:00 +0000

    fix test and change travis.yml back to build after script

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,3 @@
-# FROM node:current-slim
-
-# WORKDIR /hackney-hr-app
-# COPY package*.json /hackney-hr-app
-# RUN npm install
-
-# EXPOSE 3000
-
-# COPY . /hackney-hr-app
-# CMD [ "npm", "start"]
-
-###############################
 # pull official base image
 FROM node:current-slim
 
--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 describe("Top level App", () => {
     test('renders the title of the homepage', () => {
       render(<App />);
-      const linkElement = screen.getByText(//i);
+      const linkElement = screen.getByText(/Welcome to the Hackney Council HR System/i);
       expect(linkElement).toBeInTheDocument();
     });
 })
