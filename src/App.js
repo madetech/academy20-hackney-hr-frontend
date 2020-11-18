168bd77db7710b80063a87ae17d843c0982a95cc
Chloe <chloe.wong@madetech.com>
2020-11-18 12:27:30 +0000

    moved home section to own component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
 import HeaderBanner from "./components/headerBanner/headerBanner";
 import VisitorPage from "./pages/visitorPage/visitorPage";
 import EmployeeLogin from "./pages/employeeLoginPage/employeeLogin";
 // import EmployeeDetails from "./pages/employeeDetailsPage/employeeDetails";
-import StartButton from "./components/startButton/startButton";
+import Home from "./pages/home/home";
+
+
 
 function App() {
   return (
@@ -18,23 +20,8 @@ function App() {
               <Route exact path="/visitor" component={VisitorPage}/>
               {/* <Route exact path="/details" component={EmployeeDetails}/> */}
 
-            <section>
-              <div className="welcome-text">
-              <h1>Welcome to the Hackney Council HR System.</h1>
-              <p>If you are a visitor with a pre-booked appointment, please register your visit by clicking the button below. 
-                Once you have registered your visit, the person who you booked a visit with will soon come and collect you.</p>
-              <p>If you are an employee of Hackney Council, please login using the button below.</p>
-              </div>
-              
-              <div className="button-container"> 
-              <Link to="/visitor">
-                <StartButton text="Register your visit" />
-              </Link>
-              <Link to="/login">
-                <StartButton text="Employee login"/>
-              </Link>
-              </div>
-            </section>
+            <Home/>
+
             </Switch>
           <footer>
             <p>All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence v3.0</a>, except where otherwise stated</p>
