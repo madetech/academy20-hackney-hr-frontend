9487329e90b6bdfbe4c5e1ff733b4356d91d1e93
Chloe <chloe.wong@madetech.com>
2020-11-19 16:17:24 +0000

    added details page for sidebar implementation

---
+++ src/pages/detailsPage/detailsPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Details from "../../components/details/details";
+
+export default function DetailsPage() {
+    return (
+        <div>
+
+            <div className="nav-bar">
+                <span> My Details </span>
+                <span> Employees List </span>
+                <span> Visitors </span>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
