9430e8670f9586f2d763bf5ec59b4ce29dd0a400
Ben <ben.dalton@madetech.com>
2020-11-19 21:38:15 +0000

    add styling to nav bar on detailsPage

--- src/pages/detailsPage/detailsPage.js
+++ src/pages/detailsPage/detailsPage.js
@@ -3,14 +3,17 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Details from "../../components/details/details";
 import "../detailsPage/detailsPage.css";
 
-export default function DetailsPage(props) {
+export default function DetailsPage() {
+
     return (
         <div>
 
             <div className="nav-bar">
-                <span> My Details </span>
-                <span> Employees List </span>
-                <span> Visitors </span>
+            <div>
+                <div id="details">My Details</div>
+                <div id="employees">All Employees</div>
+                <div id="visitors">Visitors</div>
+                </div>
             </div>
             <div>
             <Details />
