1c5bba99f3662b22e4a9be3a6c7f6993bc641485
Ben <ben.dalton@madetech.com>
2020-11-21 12:27:30 +0000

    render all employees from database on details page

--- src/pages/allEmployeesPage/allEmployees.js
+++ src/pages/allEmployeesPage/allEmployees.js
@@ -1,36 +1,55 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import "./allEmployees.css";
 import axios from "axios";
+import StartButton from '../../components/startButton/startButton';
 let users = require("../../MOCK_DATA.json")
 
 export default function AllEmployees() {
     const [employee, setEmployee] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         axios.get('https://hackney-hr-backend.herokuapp.com/api/employee')
         .then(res => {
-          setEmployee(res.data);
+          setEmployee(res.data)
+          setIsLoading(false);
         })
         .catch(err => {
           console.log(err)
         })
       }, []);
 
-    return(
-        <div>
-            <section>
-                <div className="welcome-text">
-                    <h1>All Employees</h1>
-                    { employee.map(e => (
-                        <div key={e.id}>
-                            <p>{e.first_name} {e.last_name}</p>
-                            <p>{e.job_title}</p>
-                            <p>{e.office_location}</p>
-                        <br/>
-                        </div>
-                    ))}
-                </div>
-            </section>
-        </div>
-    )
+    if (isLoading) {
+        return <p>Page is loading</p>
+    } else {
+        return(
+            <Router forceRefresh={true}>
+             <div className="back">
+                <Link to="/">
+                    <StartButton text="Back to home"/>
+                </Link>
+            <div>
+            <span className="breadcrumbs">Home > All Employees</span>
+            <div className="user-details">
+                <h1>All Employees</h1>
+                { employee.map(e => (
+                    <div key={e.id}>
+                        <p><span>Name:</span> {e.first_name} {e.last_name}</p>  
+                        <p><span>Job title:</span> {e.job_title}</p>
+                        <p><span>Email:</span> {e.contact_email}</p> 
+                        <p><span>Salary:</span> Band B</p> 
+                        <p><span>Office:</span> {e.office_location}</p>
+                        <p><span>Contact address:</span> 123 Reed Street, {e.home_address_line_2}, {e.home_address_city}</p>
+                        <p><span>Next of Kin:</span> Tim Cockrell</p> 
+                    <br/>
+                    </div>
+                ))}
+            </div>
+            </div>
+            </div>
+            </Router>
+        )
+    }
 }
+
