89d337b5fbc7651ae208aa929678dc863fe1ffc0
Ben <ben.dalton@madetech.com>
2020-11-25 16:27:34 +0000

    move edit details button so it does not show on edit page

--- src/components/details/details.js
+++ src/components/details/details.js
@@ -26,70 +26,90 @@ export default function Details(props) {
     setFirstName(employee.first_name);
   }, [employee]);
 
-  const userInfo = (
-    <div>
-      <p><span>Name:</span> {employee.first_name} {employee.last_name}</p>
-      <p><span>Job title:</span> {employee.job_title}</p>
-      <p><span>Contact number:</span> {employee.contact_number}</p>
-      <p><span>Email:</span> {employee.contact_email}</p>
-      <p><span>Contact address:</span> {employee.home_address_line_1},{" "}
-        {employee.home_address_line_2}, {employee.home_address_city},{" "}
-        {employee.home_address_postcode}
-      </p>
-      <p><span>Salary:</span> {employee.salary_band}</p>
-      <p><span>Office:</span> {employee.office_location}</p>
-      <p><span>Manager:</span> {employee.manager}</p>
-      <p><span>Reportees:</span> {employee.reportees}</p>
-      <p><span>Next of Kin:</span> {employee.next_of_kin_first_name}{" "}
-        {employee.next_of_kin_last_name}
-      </p>
-      <p><span>Next of Kin contact number:</span>{" "}
-        {employee.next_of_kin_contact_number}
-      </p>
-    </div>
-  );
-
-  const handleChange = callback => e => 
-    callback(e.target.value);
-
-  // const handleSave = (e) => {
-  //   setState({ first_name: modalFirstName});    
-  //   setState({ last_name: modalLastName});                         
-  //   modalClose();
-  // };
-
   const toggleEditForm = () => {
     setMyDetails(false);
     setEditForm(true);
     console.log(myDetails);
     console.log(editForm);
   };
+
+  const userInfo = (
+    <div>
+      <Router>
+        <Link to="/edit-details">
+          <button
+            className="float-right"
+            type="button"
+            onClick={toggleEditForm}
+          >
+            Edit Details
+          </button>
+        </Link>
+      </Router>
+      <p>
+        <span>Name:</span> {employee.first_name} {employee.last_name}
+      </p>
+      <p>
+        <span>Job title:</span> {employee.job_title}
+      </p>
+      <p>
+        <span>Contact number:</span> {employee.contact_number}
+      </p>
+      <p>
+        <span>Email:</span> {employee.contact_email}
+      </p>
+      <p>
+        <span>Contact address:</span> {employee.home_address_line_1},{" "}
+        {employee.home_address_line_2}, {employee.home_address_city},{" "}
+        {employee.home_address_postcode}
+      </p>
+      <p>
+        <span>Salary:</span> {employee.salary_band}
+      </p>
+      <p>
+        <span>Office:</span> {employee.office_location}
+      </p>
+      <p>
+        <span>Manager:</span> {employee.manager}
+      </p>
+      <p>
+        <span>Reportees:</span> {employee.reportees}
+      </p>
+      <p>
+        <span>Next of Kin:</span> {employee.next_of_kin_first_name}{" "}
+        {employee.next_of_kin_last_name}
+      </p>
+      <p>
+        <span>Next of Kin contact number:</span>{" "}
+        {employee.next_of_kin_contact_number}
+      </p>
+    </div>
+  );
+
+  const handleChange = (callback) => (e) => callback(e.target.value);
+
+  // const handleSave = (e) => {
+  //   setState({ first_name: modalFirstName});
+  //   setState({ last_name: modalLastName});
+  //   modalClose();
+  // };
+
   // set state for edit details form as false, onClick set to true
   // pass state? (or prop?) to details page, if true then display the edit details form
 
   return (
     <div>
-      <span className="breadcrumbs"><a href="/">Home</a> > My Details</span>
+      <span className="breadcrumbs">
+        <a href="/">Home</a> > My Details
+      </span>
       <div className="user-details">
-        {/* <StartButton onClick={modalOpen}  /> */}
-        <Router>
-          <Link to="/edit-details">
-            <button className="float-right" type="button" onClick={toggleEditForm}>Edit Details</button>
-          </Link>
-        </Router>
-
-        {/* <a href="#" className="edit-details" onClick={modalOpen}>Edit details</a> */}
-        {/* <Modal show={modal} handleClose={modalClose}>
-            
-          </Modal> */}
-        <div>{myDetails ? userInfo : <EditDetails employee={employee}/>}</div>
+        <div>{myDetails ? userInfo : <EditDetails employee={employee} />}</div>
       </div>
     </div>
   );
-};
+}
 
-function EditDetails({employee}) {
-
+function EditDetails({ employee }) {
   const [firstName, setFirstName] = useState("");
   const [newEmployee, setNewEmployee] = useState({
     firstName: employee.first_name,
@@ -107,194 +127,197 @@ function EditDetails({employee}) {
     reportees: employee.reportees,
     nextOfKinFirstName: employee.next_of_kin_first_name,
     nextOfKinLastName: employee.next_of_kin_last_name,
-    nextOfKinContactNumber: employee.next_of_kin_contact_number
+    nextOfKinContactNumber: employee.next_of_kin_contact_number,
   });
-  const [save, setSave] = useState("")
+  const [save, setSave] = useState("");
 
-    const handleSave = (event) => {
-        event.preventDefault();
-        setSave("Details saved!");
-    }
+  const handleSave = (event) => {
+    event.preventDefault();
+    setSave("Details saved!");
+  };
   // const handleChange = callback => e => {
   //   if (callback) callback(e.target.value);
   // };
 
-  function changeEmployeeInfo(e){
+  function changeEmployeeInfo(e) {
     console.log(e);
-    setNewEmployee({...newEmployee, [e.target.name]:e.target.value});
+    setNewEmployee({ ...newEmployee, [e.target.name]: e.target.value });
     console.log(newEmployee.firstName);
   }
 
   return (
     <div>
-      <form onSubmit={handleSave} >
-      <div className="form-group">
-        <label>First Name: </label>
-        <input
-          type="text"
-          value={newEmployee.firstName}
-          name="firstName"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label>Last Name: </label>
-        <input
-          type="text"
-          value={newEmployee.lastName}
-          name="lastName"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label>Job title: </label>
-        <input
-          type="text"
-          value={newEmployee.jobTitle}
-          name="jobTitle"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label>Contact number: </label>
-        <input
-          type="number"
-          value={newEmployee.contactNumber}
-          name="contactNumber"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label>Email: </label>
-        <input
-          type="text"
-          value={newEmployee.contactEmail}
-          name="contactEmail"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label>Contact Address Line 1: </label>
-        <input
-          type="text"
-          value={newEmployee.homeAddressLine1}
-          name="homeAddressLine1"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label>Contact Address Line 2: </label>
-        <input
-          type="text"
-          value={newEmployee.homeAddressLine2}
-          name="homeAddressLine2"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-          size="10"
-        />
-      </div>
-      <div className="form-group">
-        <label>Contact Address Line City: </label>
-        <input
-          type="text"
-          value={newEmployee.homeAddressCity}
-          name="homeAddressCity"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label>Contact Address Postcode: </label>
-        <input
-          type="text"
-          value={newEmployee.homeAddressPostcode}
-          name="homeAddressPostcode"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label>Salary Band: </label>
-        <input
-          type="text"
-          value={newEmployee.salaryBand}
-          name="salaryBand"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label>Manager: </label>
-        <input
-          type="text"
-          value={newEmployee.manager}
-          name="manager"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-        />
-      </div>
-      <div className="form-group">
-        <label>Reportees: </label>
-        <input
-          type="text"
-          value={newEmployee.reportees}
-          name="reportees"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-        />
-      </div>
-      <div className="form-group">
-        <label>Next of Kin First Name: </label>
-        <input
-          type="text"
-          value={newEmployee.nextOfKinFirstName}
-          name="nextOfKinFirstName"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label>Next of Kin Last Name: </label>
-        <input
-          type="text"
-          value={newEmployee.nextOfKinLastName}
-          name="nextOfKinLastName"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label>Next of Kin Contact Number: </label>
-        <input
-          type="number"
-          value={newEmployee.nextOfKinContactNumber}
-          name="nextOfKinContactNumber"
-          onChange={changeEmployeeInfo}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-      </div>
-      <SubmitButton/><div>{save}</div>
+      <form onSubmit={handleSave}>
+        <div className="form-group">
+          <label>First Name: </label>
+          <input
+            type="text"
+            value={newEmployee.firstName}
+            name="firstName"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Last Name: </label>
+          <input
+            type="text"
+            value={newEmployee.lastName}
+            name="lastName"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Job title: </label>
+          <input
+            type="text"
+            value={newEmployee.jobTitle}
+            name="jobTitle"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Contact number: </label>
+          <input
+            type="number"
+            value={newEmployee.contactNumber}
+            name="contactNumber"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Email: </label>
+          <input
+            type="text"
+            value={newEmployee.contactEmail}
+            name="contactEmail"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Contact Address Line 1: </label>
+          <input
+            type="text"
+            value={newEmployee.homeAddressLine1}
+            name="homeAddressLine1"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Contact Address Line 2: </label>
+          <input
+            type="text"
+            value={newEmployee.homeAddressLine2}
+            name="homeAddressLine2"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+            size="10"
+          />
+        </div>
+        <div className="form-group">
+          <label>Contact Address Line City: </label>
+          <input
+            type="text"
+            value={newEmployee.homeAddressCity}
+            name="homeAddressCity"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Contact Address Postcode: </label>
+          <input
+            type="text"
+            value={newEmployee.homeAddressPostcode}
+            name="homeAddressPostcode"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Salary Band: </label>
+          <input
+            type="text"
+            value={newEmployee.salaryBand}
+            name="salaryBand"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Manager: </label>
+          <input
+            type="text"
+            value={newEmployee.manager}
+            name="manager"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label>Reportees: </label>
+          <input
+            type="text"
+            value={newEmployee.reportees}
+            name="reportees"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label>Next of Kin First Name: </label>
+          <input
+            type="text"
+            value={newEmployee.nextOfKinFirstName}
+            name="nextOfKinFirstName"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Next of Kin Last Name: </label>
+          <input
+            type="text"
+            value={newEmployee.nextOfKinLastName}
+            name="nextOfKinLastName"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Next of Kin Contact Number: </label>
+          <input
+            type="number"
+            value={newEmployee.nextOfKinContactNumber}
+            name="nextOfKinContactNumber"
+            onChange={changeEmployeeInfo}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group"></div>
+        <SubmitButton />
+        <div>{save}</div>
       </form>
+      <span className="details-back">
+        <a href="/details"> {"<"} Back</a>
+      </span>
       {/* <span>
       <Router>
       <Link to="/details">
@@ -303,6 +326,5 @@ function EditDetails({employee}) {
         </Router>
         </span> */}
     </div>
-  )
+  );
 }
-
